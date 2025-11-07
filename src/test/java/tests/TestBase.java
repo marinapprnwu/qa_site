f1f5a89818859e8aae76a0814f0088af39bfc843
Marina Vasileva <marisha071997@gmail.com>
2025-11-07 18:32:26 +0300

    add allure-selenide

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,17 +1,21 @@
 package tests;
 
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
+
     static final String siteUrl = "https://arenarussia.ru";
     static SelenideElement regionPopup = $("div.popup-region2").$(byText("ДА"));
 
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         open(siteUrl);
 //       ToDo вынести селектр в отдельный класс
         regionPopup.click();
